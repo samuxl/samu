321151a12474ade10a737f776224a0f393b9fe2d
css bear <cssbear@outlook.com>
2023-10-19 15:51:02 +0800

    Samu edit 231019-01

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.DateTime = DateTime.Now.ToString();
             return View();
         }
 
